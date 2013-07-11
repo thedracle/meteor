7fc9af46911533bb0ff38a98fe9f083f2a148923
jthomas <jthomas@jthomas-VirtualBox.(none)>
2013-07-11 13:31:35 -0600

    Test with minify reintroduced.

--- tools/meteor.js
+++ tools/meteor.js
@@ -608,7 +608,7 @@ Fiber(function () {
       var bundler = require(path.join(__dirname, 'bundler.js'));
       var errors = bundler.bundle(context.appDir, bundle_path, {
         nodeModulesMode: 'copy',
-        minify: false, // JRT: Why does this not obey --debug? So sad.  // XXX allow --debug
+        minify: true, // XXX allow --debug
         releaseStamp: context.releaseVersion,
         packageSearchOptions: context.packageSearchOptions
       });
